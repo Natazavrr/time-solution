cc8e9333f949c5c345818a14ea5002935ee1082f
nataliiia11 <borshiq111@gmail.com>
2022-04-27 21:57:19 +0200

    Fixed typo in hours

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
         public static void main(String[] args) {
-            System.out.println(timeTranslate(7200));
+            System.out.println(timeTranslate(3600));
 
         }
 
@@ -40,11 +40,11 @@ public class Main {
 
             // delete commas at the end if other time units equal zero
             if(yrs == 0 && days == 0 && hrs == 0 && min == 0 && scnd != 1) {
-                scndStr = scnd + " seconds ";
+                scndStr = scnd + " seconds";
             }
 
            if(yrs == 0 && days == 0 && hrs == 0 && min == 0 && scnd == 1) {
-               scndStr = scnd + " second ";
+               scndStr = scnd + " second";
            }
 
            if(yrs != 0 && days == 0 && hrs == 0 && min == 0 && scnd == 0) {
@@ -64,11 +64,11 @@ public class Main {
             }
 
             if(yrs == 0 && days == 0 && hrs != 0 && min == 0 && scnd == 0) {
-                hrsStr = hrs + " hour";
+                hrsStr = hrs + " hours";
             }
 
-            if(yrs == 0 && days == 0 && hrs != 1 && min == 0 && scnd == 0) {
-                hrsStr = hrs + " hours";
+            if(yrs == 0 && days == 0 && hrs == 1 && min == 0 && scnd == 0) {
+                hrsStr = hrs + " hour";
             }
 
             if(yrs == 0 && days == 0 && hrs == 0 && min != 0 && scnd == 0) {
